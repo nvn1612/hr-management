f9a6fb9eb5530e66877963be50c08232877cdc65
nvn1612 <2051063947@e.tlu.edu.vn>
2024-05-10 09:55:38 +0700

    update departments

--- src/libs/react-router/router.tsx
+++ src/libs/react-router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { Dashboard, Login } from "src/pages";
 import { UserInfo } from "src/pages/dashboard/user-info";
 import { Accounts } from "src/pages/dashboard/accounts";
+import { Departments } from "src/pages/dashboard/departments";
 import App from "src/App";
 
 export const router = createBrowserRouter([
@@ -18,6 +19,10 @@ export const router = createBrowserRouter([
       {
         path: "accounts",
         element: <Accounts />,
+      },
+      {
+        path: "departments",
+        element: <Departments />,
       },
     ],
   },
