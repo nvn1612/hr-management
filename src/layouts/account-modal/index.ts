aa598a123f6a43de9e3e966041dedd5d1b98f8a6
Chi Cong <chicong422002@gmail.com>
2024-05-13 21:50:19 +0700

    update filter and create user UI

---
+++ src/layouts/account-modal/index.ts
@@ -0,0 +1,2 @@
+export * from "./account-modal";
+export * from "../user-info-form";
--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -2,3 +2,4 @@ export * from "src/layouts/dashboard-sidebar";
 export * from "src/layouts/login-form";
 export * from "src/layouts/user-advance";
 export * from "src/layouts/user-info-form";
+export * from "src/layouts/account-modal";
