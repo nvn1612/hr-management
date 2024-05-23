c46c2cc2db6d2e50894273c8b6818fd2f60ed196
Chi Cong <chicong422002@gmail.com>
2024-05-23 19:23:30 +0700

    add local storage utils. tempory use local storage instead of cookie to save token

--- src/share/services/accountServices/accountServices.ts
+++ src/share/services/accountServices/accountServices.ts
@@ -1,36 +1,8 @@
-import axios from "axios";
-import { loginApi } from "src/share/apis";
-import Cookies from "js-cookie";
 import { hrManagementApi } from "src/share/services/baseApi";
 import { User, Response, LoginResp } from "src/share/models";
 
-import type { LoginFieldType, LoginReqBody } from "src/layouts/login-form";
+import type { LoginReqBody } from "src/layouts/login-form";
 import type {} from "src/pages/login";
-import type { loginResponse } from "./models";
-
-export const loginService = async (attemptLogin: LoginFieldType) => {
-  try {
-    const response = await axios<loginResponse>({
-      method: "post",
-      url: loginApi,
-      data: {
-        username: attemptLogin.username,
-        password: attemptLogin.password,
-      },
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "*/*",
-      },
-      timeout: 5000,
-    });
-    Cookies.set("auth-token", response.data.data.token, {
-      expires: 1,
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error();
-  }
-};
 
 const accountServices = hrManagementApi.injectEndpoints({
   endpoints: (build) => ({
