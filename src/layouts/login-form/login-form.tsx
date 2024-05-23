b47548d3fc4d8b88e92875f2c7137925aa59f32d
Chi Cong <chicong422002@gmail.com>
2024-05-23 15:58:08 +0700

    merging api

--- src/layouts/login-form/login-form.tsx
+++ src/layouts/login-form/login-form.tsx
@@ -1,39 +1,44 @@
 import "./login-form.css";
 import { AntdButton } from "src/components/antd-button";
-import { ScreenBlocker } from "src/components/screen-blocker";
 import { Form, Input, Checkbox, message } from "antd";
 import type { FormProps } from "antd";
-import { loginService } from "src/share/services/accountServices";
+import { useLoginMutation } from "src/share/services/accountServices";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 
 export type LoginFieldType = {
   username?: string;
   password?: string;
   remember?: boolean;
 };
+export type LoginReqBody = {
+  email?: string;
+  password?: string;
+};
 
 export const LoginForm = () => {
   const navigate = useNavigate();
   const [messageApi, contextHolder] = message.useMessage();
-  const [loadingState, setLoadingState] = useState<boolean>(false);
+  const [loginService] = useLoginMutation();
 
   const onFinish: FormProps<LoginFieldType>["onFinish"] = async (values) => {
-    setLoadingState(true);
-    const responseData = await loginService(values);
-    if (responseData.flag) {
-      navigate("/dashboard");
-      messageApi.success("success");
-    } else {
-      messageApi.error("error");
-    }
-    setLoadingState(false);
+    await loginService({
+      email: values.username,
+      password: values.password,
+    } as LoginReqBody)
+      .then((resp) => {
+        if (!resp.error) {
+          navigate("/dashboard");
+          messageApi.success("success");
+        }
+      })
+      .catch((e) => {
+        messageApi.error(e);
+      });
   };
 
   return (
     <>
       {contextHolder}
-      {loadingState && <ScreenBlocker />}
       <Form
         name='login'
         onFinish={onFinish}
