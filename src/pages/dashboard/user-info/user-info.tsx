a52c69392555354d71bf29baa507ec8a8537c35c
Chi Cong <chicong422002@gmail.com>
2024-05-28 01:57:46 +0700

    user role advance tab

--- src/pages/dashboard/user-info/user-info.tsx
+++ src/pages/dashboard/user-info/user-info.tsx
@@ -1,31 +1,33 @@
 import "./user-info.css";
-import { Avatar } from "antd";
+import { Avatar, Spin } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import { UserInfoForm } from "src/layouts/";
 import { useGetUserDetailQuery } from "src/share/services";
 
 export const UserInfo = () => {
-  const { data } = useGetUserDetailQuery();
+  const { data, isLoading } = useGetUserDetailQuery();
 
   return (
-    <div className='user-info-container'>
-      <div className='first-section'>
-        <Avatar size={128} icon={<UserOutlined />} />
-        <h3>Account's username</h3>
-        <h3>Staff</h3>
+    <Spin size='large' tip='Loading User Detail' spinning={isLoading}>
+      <div className='user-info-container'>
+        <div className='first-section'>
+          <Avatar size={128} icon={<UserOutlined />} />
+          <h3>Account's username</h3>
+          <h3>Staff</h3>
+        </div>
+        <UserInfoForm
+          {...(data && {
+            initValues: {
+              username: data.data.username,
+              name: data.data.name,
+              email: data.data.email,
+              phone: data.data.phone,
+              birthday: data.data.birthday,
+            },
+          })}
+          action='detail'
+        />
       </div>
-      <UserInfoForm
-        {...(data && {
-          initValues: {
-            username: data.data.username,
-            name: data.data.name,
-            email: data.data.email,
-            phone: data.data.phone,
-            birthday: data.data.birthday,
-          },
-        })}
-        action='detail'
-      />
-    </div>
+    </Spin>
   );
 };
