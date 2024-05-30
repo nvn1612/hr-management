6dd2f6aef4c991c2ab5eb4303938d63f9637ea4b
Chi Cong <chicong422002@gmail.com>
2024-05-31 01:09:36 +0700

    add route protector

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,18 +1,29 @@
 import "./dashboard.css";
 import { Layout } from "antd";
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import { Dashboardsidebar } from "src/layouts/dashboard-sidebar";
+
+import { localStorageUtil } from "src/share/utils";
+
 export const Dashboard = () => {
+  const accessToken = localStorageUtil.get("accessToken");
   const { Content } = Layout;
 
   return (
-    <Layout>
-      <Dashboardsidebar />
-      <Layout>
-        <Content className='content'>
-          <Outlet />
-        </Content>
-      </Layout>
-    </Layout>
+    <>
+      {accessToken ? (
+        <Layout>
+          <Dashboardsidebar />
+          <Layout>
+            <Content className='content'>
+              <Navigate to={"/dashboard/user-info"} />
+              <Outlet />
+            </Content>
+          </Layout>
+        </Layout>
+      ) : (
+        <Navigate to={"/login"} replace />
+      )}
+    </>
   );
 };
