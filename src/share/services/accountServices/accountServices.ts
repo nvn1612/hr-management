0fbe0007683cb398dc97abc7abe24cad5033671a
Chi Cong <chicong422002@gmail.com>
2024-06-05 00:17:21 +0700

    add user tag for getDeparmentStaff

--- src/share/services/accountServices/accountServices.ts
+++ src/share/services/accountServices/accountServices.ts
@@ -180,6 +180,7 @@ const accountServices = hrManagementApi.injectEndpoints({
           },
         };
       },
+      providesTags: ["User"],
       transformResponse: (response: Response<GetUserResp>) => response.data,
     }),
   }),
