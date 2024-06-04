f332c36a2de1f09b8057b494868da82ee8f23383
nvn1612 <2051063947@e.tlu.edu.vn>
2024-06-04 14:14:55 +0700

    Revert "update departments"
    This reverts commit c2a3b997070b10d842ccdbf175b9bb097c623010.

--- src/layouts/modal-departments/modal-list-staff-department/modal-list-staff-department.tsx
+++ src/layouts/modal-departments/modal-list-staff-department/modal-list-staff-department.tsx
@@ -3,13 +3,11 @@ import { Modal, Table, } from "antd";
 import { MinusCircleOutlined, PlusSquareOutlined } from '@ant-design/icons';
 import { ModalAddStaffsDepartment } from "src/layouts/modal-departments/modal-add-staffs-department";
 import "./modal-list-staff-department.css";
-import { Department } from "src/share/interfaces/department";
-import { useGetDepartmentStaffsQuery } from "src/share/services";
+import {}
 
 type ModalDepartmentListStaffProps = {
   visible: boolean;
   setVisible: React.Dispatch<React.SetStateAction<boolean>>;
-  department?: Department;
 };
 
 const columns = [
@@ -24,9 +22,9 @@ const columns = [
     key: "username",
   },
   {
-    title: "email",
-    dataIndex: "email",
-    key: "email",
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
   },
   {
     title: "Action",
@@ -35,20 +33,46 @@ const columns = [
   },
 ];
 
-
 export const ModalListStaffDepartment = ({
   visible,
   setVisible,
-  department
 }: ModalDepartmentListStaffProps) => {
   const [visibleAddStaff, setVisibleAddStaff] = useState(false);
   const showModalAddStaff = () => {
     setVisibleAddStaff(true);
   }
-  
-
-  const {data} = useGetDepartmentStaffsQuery({departmentId:department?.department_id});
-
+  const data = [
+    {
+      key: "1",
+      name: "Van Diep Doan",
+      username: "vanđiepdoan123",
+      role: "manager",
+    },
+    {
+      key: "2",
+      name: "Nguyen Van A",
+      username: "vana123",
+      role: "staff",
+    },
+    {
+      key: "3",
+      name: "Nguyen Van B",
+      username: "vanb123",
+      role: "staff",
+    },
+    {
+      key: "4",
+      name: "Nguyen Van C",
+      username: "vanc123",
+      role: "staff",
+    },
+    {
+      key: "5",
+      name: "Nguyen Van D",
+      username: "vanc123",
+      role: "staff",
+    },
+  ];
   return (
     <>
       <Modal
@@ -60,13 +84,7 @@ export const ModalListStaffDepartment = ({
       >
         <Table
           columns={columns}
-          dataSource={data?.users.map((user) => ({
-            key: user.user_id,
-            name: user.name,
-            username: user.username,
-            email: user.email,
-          }))
-          }
+          dataSource={data}
           pagination={{ pageSize: 4 }}
         />
         <PlusSquareOutlined className="icon-add-staffs" onClick={showModalAddStaff}/>
