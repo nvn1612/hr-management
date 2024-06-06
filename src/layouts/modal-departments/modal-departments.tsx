ae9cf3d4632d8f8f7dabd566118f55ba2daf8d81
Chi Cong <chicong422002@gmail.com>
2024-06-06 12:07:32 +0700

    update department report

--- src/layouts/modal-departments/modal-departments.tsx
+++ src/layouts/modal-departments/modal-departments.tsx
@@ -24,10 +24,11 @@ import { ModalReportProjectDepartment } from "src/layouts/modal-departments/moda
 import { Department } from "src/share/models/departmentModels";
 import { Project } from "src/share/models/projectModels";
 import { useDeleteDepartmentsMutation } from "src/share/services";
-import { useGetReportDepartmentsQuery } from 'src/share/services/departmentServices'
+import { useGetReportDepartmentsQuery } from "src/share/services/departmentServices";
 import { useGetAllProjectDepartmentQuery } from "src/share/services/departmentServices";
 import { ProjectCard } from "src/components/project-card";
 import "./modal-departments.css";
+import { useHandleReports } from "src/share/hooks";
 
 type ModalDepartmentsProps = {
   visible: boolean;
@@ -35,116 +36,10 @@ type ModalDepartmentsProps = {
   department?: Department;
 };
 
-
-const reportProject =
-{
-  "12-5-2024": [
-    {
-      "DA6582": [
-        {
-          "task1": [
-            {
-              "activity-1": "nguyen van A"
-            },
-            {
-              "activity-2": "nguyen van B"
-            }
-          ]
-        },
-        {
-          "task2": [
-            {
-              "activity-3": "nguyen van A"
-            },
-            {
-              "activity-4": "nguyen van B"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "DA6583": [
-        {
-          "task1": [
-            {
-              "activity-1": "nguyen van A"
-            },
-            {
-              "activity-2": "nguyen van B"
-            }
-          ]
-        },
-        {
-          "task2": [
-            {
-              "activity-3": "nguyen van A"
-            },
-            {
-              "activity-4": "nguyen van B"
-            }
-          ]
-        }
-      ]
-    },
-  ],
-  "6-6-2024": [
-    {
-      "DA6582": [
-        {
-          "task1": [
-            {
-              "activity-1": "nguyen van A"
-            },
-            {
-              "activity-2": "nguyen van B"
-            }
-          ]
-        },
-        {
-          "task2": [
-            {
-              "activity-3": "nguyen van A"
-            },
-            {
-              "activity-4": "nguyen van B"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "DA6583": [
-        {
-          "task1": [
-            {
-              "activity-1": "nguyen van A"
-            },
-            {
-              "activity-2": "nguyen van B"
-            }
-          ]
-        },
-        {
-          "task2": [
-            {
-              "activity-3": "nguyen van A"
-            },
-            {
-              "activity-4": "nguyen van B"
-            }
-          ]
-        }
-      ]
-    },
-  ]
-};
-
-
 export const ModalDepartments = ({
   visible,
   setVisible,
-  department
+  department,
 }: ModalDepartmentsProps) => {
   const onChange = (key: string) => {
     console.log(key);
@@ -169,38 +64,44 @@ export const ModalDepartments = ({
 
   const [deleteDepartment] = useDeleteDepartmentsMutation();
   const handleDeleteDepartment = async () => {
-    await deleteDepartment({ departmentId: department?.department_id }).unwrap().then().catch()
-  }
-  const { data: reportData } = useGetReportDepartmentsQuery({ departmentId: department?.department_id })
-  const { data: projectData } = useGetAllProjectDepartmentQuery({ departmentId: department?.department_id })
+    await deleteDepartment({ departmentId: department?.department_id })
+      .unwrap()
+      .then()
+      .catch();
+  };
+  const { data: reportData } = useGetReportDepartmentsQuery({
+    departmentId: department?.department_id,
+  });
+  const { data: projectData } = useGetAllProjectDepartmentQuery({
+    departmentId: department?.department_id,
+  });
+  const reportTimelineItem = useHandleReports("department", reportData);
   const items: TabsProps["items"] = [
     {
       key: "1",
       label: "Information",
       children: (
         <>
-          <div className="information-departments">
-            <p>
-              {department?.description}
-            </p>
-            <div className="info-department-wrapper">
-              <div className="department-manager">
+          <div className='information-departments'>
+            <p>{department?.description}</p>
+            <div className='info-department-wrapper'>
+              <div className='department-manager'>
                 <Row>
                   <Col span={10}>
-                    <div className="title-manager-department">
+                    <div className='title-manager-department'>
                       <span>Manager</span>
                     </div>
                   </Col>
                   <Col span={12}>
                     <div
-                      className="name-manager-department"
+                      className='name-manager-department'
                       onClick={showModal}
                     >
                       <Avatar icon={<UserOutlined />} size={25} />
                       <p>{department?.information?.manager?.username}</p>
                     </div>
                   </Col>
-                  <Col span={2} className="edit-manager-icon">
+                  <Col span={2} className='edit-manager-icon'>
                     <div onClick={showAddManager}>
                       <EditOutlined />
                     </div>
@@ -208,28 +109,28 @@ export const ModalDepartments = ({
                 </Row>
               </div>
             </div>
-            <div className="info-staffs-wrapper">
-              <div className="department-staff">
+            <div className='info-staffs-wrapper'>
+              <div className='department-staff'>
                 <Row>
                   <Col span={10}>
-                    <div className="title-staff-department">
+                    <div className='title-staff-department'>
                       <span>Staff</span>
                     </div>
                   </Col>
                   <Col span={12}>
-                    <div className="number-staff-department">
+                    <div className='number-staff-department'>
                       <p>{department?.information?.total_staff}</p>
                     </div>
                   </Col>
-                  <Col span={2} className="icon-list-staff-department">
+                  <Col span={2} className='icon-list-staff-department'>
                     <UnorderedListOutlined onClick={showModalStaff} />
                   </Col>
                 </Row>
               </div>
             </div>
-            <div className="delete-icon">
+            <div className='delete-icon'>
               <Popconfirm
-                title="Are you sure to delete this department?"
+                title='Are you sure to delete this department?'
                 icon={<QuestionCircleOutlined style={{ color: "red" }} />}
                 onConfirm={() => {
                   handleDeleteDepartment().then(() => {
@@ -248,81 +149,16 @@ export const ModalDepartments = ({
       key: "2",
       label: "Projects",
       children: projectData?.data.map((project: Project, index: number) => (
-        <ProjectCard onClick={() => console.log('hello world')} projectName={project.name} />
+        <ProjectCard
+          onClick={() => console.log("hello world")}
+          projectName={project.name}
+        />
       )),
     },
     {
       key: "3",
       label: "Report",
-      children: (
-        <Timeline
-          mode={"alternate"}
-          items={[
-            {
-              color: "black",
-              label: "2024-05-03",
-              children: (
-                <>
-                  <Tag onClick={showReportProject}>DA6582</Tag>
-                  <br />
-                  <strong>Design UI</strong>
-                  <p>
-                    Design UI mockup 3 screens: home, detail, report - by Van
-                    Tuan Tran
-                  </p>
-                  <p>Design UI mockup login screen - by Quoc Chinh Nguyen</p>
-                  <strong>Develop mobile application</strong>
-                  <p>Develop authentication feature - by The Hieu Pham</p>
-                  <Tag onClick={showReportProject}>DA8642</Tag>
-                  <br />
-                  <strong>Develop algorithm</strong>
-                  <p>Develop authentication feature - by The Hieu Pham</p>
-                </>
-              ),
-            },
-            {
-              color: "red",
-              label: "2024-05-02",
-              children: (
-                <>
-                  <Tag onClick={showReportProject}>DA6582</Tag>
-                  <br />
-                  <strong>Design UI</strong>
-                  <p>
-                    Design UI mockup 3 screens: home, detail, report - by Van
-                    Tuan Tran
-                  </p>
-                  <p>Design UI mockup login screen - by Quoc Chinh Nguyen</p>
-                  <strong>Develop mobile application</strong>
-                  <p>Develop authentication feature - by The Hieu Pham</p>
-                  <Tag onClick={showReportProject}>DA8642</Tag>
-                  <br />
-                  <strong>Develop algorithm</strong>
-                  <p>Develop authentication feature - by The Hieu Pham</p>
-                </>
-              ),
-            },
-            {
-              color: "green",
-              label: "2024-04-26",
-              children: (
-                <>
-                  <Tag onClick={showReportProject}>DA6582</Tag>
-                  <br />
-                  <strong>Design UI</strong>
-                  <p>
-                    Design UI mockup 3 screens: home, detail, report - by Van
-                    Tuan Tran
-                  </p>
-                  <p>Design UI mockup login screen - by Quoc Chinh Nguyen</p>
-                  <strong>Develop mobile application</strong>
-                  <p>Develop authentication feature - by The Hieu Pham</p>
-                </>
-              ),
-            },
-          ]}
-        />
-      ),
+      children: <Timeline mode={"alternate"} items={reportTimelineItem} />,
     },
   ];
   return (
@@ -334,7 +170,7 @@ export const ModalDepartments = ({
         title={department?.name}
         width={1000}
       >
-        <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+        <Tabs defaultActiveKey='1' items={items} onChange={onChange} />
       </Modal>
       <ModalDepartmentManager
         visible={managerModalVisible}
--- src/share/hooks/useHandleReports.tsx
+++ src/share/hooks/useHandleReports.tsx
@@ -1,28 +1,29 @@
 import dayjs from "dayjs";
 import { ReactNode, useEffect, useState, useCallback } from "react";
 import type { ProjectReportResp } from "src/share/models/projectModels";
-import { Typography } from "antd";
+import { Typography, Tag } from "antd";
 
 import type { TimelineItemProps } from "antd";
 import { Activity } from "src/share/models";
 
 interface PrepareReports {
   taskDesc?: string;
+  projectCode?: string;
   date?: string;
   activities?: Activity[];
 }
 
 export const useHandleReports = (
   type: "department" | "project",
-  reports: ProjectReportResp | undefined
+  reports: ProjectReportResp | ProjectReportResp[] | undefined
 ) => {
   const [timelineItem, setTimelineItem] = useState<TimelineItemProps[]>();
   const { Text } = Typography;
 
   const handleReport = useCallback(() => {
+    const newReportList: PrepareReports[] = [];
     if (type === "project" && reports) {
-      const newReportList: PrepareReports[] = [];
-      reports.tasks.forEach((task) => {
+      (reports as ProjectReportResp).tasks.forEach((task) => {
         for (const date in task.activities) {
           const newReport: PrepareReports = {};
           newReport.taskDesc = task.description;
@@ -74,11 +75,75 @@ export const useHandleReports = (
       });
       setTimelineItem(finalTimeline);
     }
-  }, [reports]);
+
+    if (type === "department" && reports) {
+      const finalTimeline: TimelineItemProps[] = [];
+      (reports as ProjectReportResp[]).forEach((projectReport) => {
+        projectReport.tasks.forEach((task) => {
+          for (const date in task.activities) {
+            const newReport: PrepareReports = {};
+            newReport.projectCode = projectReport.projectCode;
+            newReport.taskDesc = task.description;
+            newReport.date = date;
+            newReport.activities = task.activities[date];
+            newReportList.push(newReport);
+          }
+        });
+
+        const sortedList = newReportList.sort(
+          (a, b) =>
+            dayjs(b.date as string).millisecond() -
+            dayjs(a.date as string).millisecond()
+        );
+
+        let timeItem: TimelineItemProps = {};
+        const tempTimeChildren: ReactNode[] = [];
+        sortedList?.forEach((report, index) => {
+          if (index === 0) {
+            timeItem.label = report.date;
+          }
+          if (index !== 0 && sortedList[index - 1].date !== report.date) {
+            timeItem.children = tempTimeChildren.map((timeChild) => timeChild);
+            tempTimeChildren.length = 0;
+            finalTimeline.push(timeItem);
+            timeItem = {};
+            timeItem.label = report.date;
+          }
+          tempTimeChildren.push(
+            <div className='project-report-detail'>
+              {(index !== 0 &&
+                sortedList[index - 1].projectCode !== report.projectCode) ||
+              index === 0 ? (
+                <Tag>{report.projectCode}</Tag>
+              ) : (
+                ""
+              )}
+              <Text style={{ fontWeight: 700 }}>{report.taskDesc}</Text>
+              {report.activities &&
+                report.activities.map((activity) => {
+                  return (
+                    <>
+                      <span style={{ color: "#8c8c8c", fontWeight: 500 }}>
+                        {activity.description}
+                      </span>
+                    </>
+                  );
+                })}
+            </div>
+          );
+          if (index === sortedList.length - 1) {
+            timeItem.children = tempTimeChildren.map((timeChild) => timeChild);
+            finalTimeline.push(timeItem);
+          }
+        });
+      });
+      setTimelineItem(finalTimeline);
+    }
+  }, [Text, reports, type]);
 
   useEffect(() => {
     handleReport();
-  }, [reports]);
+  }, [handleReport, reports]);
 
   return timelineItem;
 };
