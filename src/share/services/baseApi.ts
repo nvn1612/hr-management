af95261389cca3cc46ed4dea854cab5894880ca2
chicong <chicong422002@gamil.com>
2024-06-07 10:06:21 +0700

    fix useHandleReport

--- src/share/services/baseApi.ts
+++ src/share/services/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const hrManagementApi = createApi({
   reducerPath: "hrManagementApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3050/",
+    baseUrl: "http://10.100.1.153:3050/",
   }),
   tagTypes: [
     "User",
