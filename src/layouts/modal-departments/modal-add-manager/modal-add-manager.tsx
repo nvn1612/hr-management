ff25f157ac5904b38f3bec1283d124dfbeb31115
nvn1612 <2051063947@e.tlu.edu.vn>
2024-06-07 10:26:36 +0700

    fix render data build-departments

--- src/layouts/modal-departments/modal-add-manager/modal-add-manager.tsx
+++ src/layouts/modal-departments/modal-add-manager/modal-add-manager.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Modal, Col, Divider, Row, Avatar, Checkbox } from "antd";
+import { Modal, Col, Divider, Row, Avatar, Checkbox, message } from "antd";
 import "./modal-add-manager.css";
 import { useGetUsersQuery } from "src/share/services";
-import { useUpdateManagerDepartmentMutation } from 'src/share/services/departmentServices';
+import { useUpdateManagerDepartmentMutation } from "src/share/services/departmentServices";
 import { Department } from "src/share/models";
 
 type ModalAddManagerProps = {
@@ -14,11 +14,10 @@ type ModalAddManagerProps = {
 export const ModalAddManager = ({
   visible,
   setVisible,
-  department
+  department,
 }: ModalAddManagerProps) => {
-
   const [mainManager, setMainManager] = useState<string | undefined>();
-  const {data} = useGetUsersQuery({role:"MANAGER"});
+  const { data } = useGetUsersQuery({ role: "MANAGER" });
   const [selectedManager, setSelectedManager] = useState<number | null>(null);
   const [updateManager] = useUpdateManagerDepartmentMutation();
 
@@ -28,12 +27,22 @@ export const ModalAddManager = ({
   };
 
   const handleUpdateManager = async () => {
-      await updateManager({departmentId: department?.department_id, managerId: mainManager});
-      setVisible(false);
+    await updateManager({
+      departmentId: department?.department_id,
+      managerId: mainManager,
+    })
+      .unwrap()
+      .then((value) => {
+        message.success("Manager Updated");
+      })
+      .catch(() => {
+        message.error("Failed to update manager");
+      });
+    setVisible(false);
   };
 
   const handleCancel = () => {
-    resetModalState(); 
+    resetModalState();
     setVisible(false);
   };
 
@@ -42,7 +51,11 @@ export const ModalAddManager = ({
       <Modal
         title="Select Manager"
         visible={visible}
-        onOk={handleUpdateManager}
+        onOk={()=>{
+          handleUpdateManager();
+          setSelectedManager(null);
+        }
+          }
         onCancel={handleCancel}
         width={800}
         className="modal-select-manager"
@@ -57,23 +70,29 @@ export const ModalAddManager = ({
               List Manager
             </Divider>
             <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-              {data?.users.filter(user => user.UserProperty?.department_id === null).map((manager, index) => (
-                <Col className="gutter-row" span={6} key={index}>
-                  <div className="item-list-manager"onClick={() => {setSelectedManager(selectedManager === index ? null : index);
-                    setMainManager(manager.user_id)}
-                  }>
-                    <div className="checkbox-select-staff">
-                    <Checkbox 
-                      checked={selectedManager === index}
-                    />
+              {data?.users
+                .filter((user) => user.UserProperty?.department_id === null)
+                .map((manager, index) => (
+                  <Col className="gutter-row" span={6} key={index}>
+                    <div
+                      className="item-list-manager"
+                      onClick={() => {
+                        setSelectedManager(
+                          selectedManager === index ? null : index
+                        );
+                        setMainManager(manager.user_id);
+                      }}
+                    >
+                      <div className="checkbox-select-staff">
+                        <Checkbox checked={selectedManager === index} />
+                      </div>
+                      <div className="avatar-manager">
+                        <Avatar size={64} src={manager.avatar} />
+                      </div>
+                      <div className="name-manager">{manager.name}</div>
                     </div>
-                    <div className="avatar-manager">
-                      <Avatar size={64} src={manager.avatar}/>
-                    </div>
-                    <div className="name-manager">{manager.name}</div>
-                  </div>
-                </Col>
-              ))}
+                  </Col>
+                ))}
             </Row>
           </div>
         </div>
