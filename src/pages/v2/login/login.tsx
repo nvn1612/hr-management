6867308f4f1b175c1fafa1c832dad2702833d9d7
chicong <chicong422002@gamil.com>
2024-07-24 17:27:33 +0700

    update login page

--- src/pages/v2/login/login.tsx
+++ src/pages/v2/login/login.tsx
@@ -1,15 +1,16 @@
 import "./login.css";
 import { LoginForm } from "src/layouts/v2";
 import { Typography } from "antd";
-import { DocumentSection } from "src/components/v2";
 
 export const Login = () => {
   return (
     <div className='v2-login-page'>
-      <div className='login-form-container'>
-        <DocumentSection />
-        <Typography.Title level={2}>LOGIN</Typography.Title>
-        <LoginForm />
+      <div className='content-container'>
+        <div className='login-form-container'>
+          <h2>Welcome to</h2>
+          <h2 className='project-manager-title'>Project Management</h2>
+          <LoginForm />
+        </div>
       </div>
     </div>
   );
